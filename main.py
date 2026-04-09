cfd2d849adaf945ef9fac88614ffa7e5d1db0b06
Stefi <stefanocampostrini123@gmail.com>
2026-04-10 00:37:06 +0200

    Intento de mejora
    no funciono, pero es una base...

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import moviepy
 from moviepy import concatenate_videoclips
 import os
+import psutil
 
 
-clipsPath = r"C:\Users\retra\OneDrive\Escritorio\Main\Code\clips"
+clipsPath = ""
 transitionVideo = moviepy.VideoFileClip(os.path.join(r"C:\Users\retra\OneDrive\Escritorio\Main\Code\lazy-clip\transition.mp4")).resized(new_size=(1920, 1080))
-clipsRaw = os.listdir(clipsPath)
-clips = []
+threadsAvailable = psutil.cpu_count() - 1 #Cuantos hilos tiene la PC menos uno
 
-def render_video():
+
+print(f"Se van a usar {threadsAvailable} hilos")
+def render_video(logger):
+    clips = []
+    clipsRaw = os.listdir(clipsPath) #OJO, SI NO HAY UN PATH MARCA ERROR, HAY QUE SOLUCIONARLO
+
     for i,clip in enumerate(clipsRaw):
         if (clip.endswith(".mp4") or clip.endswith(".wav")):
             ruta_completa = os.path.join(clipsPath, clip) 
@@ -18,14 +23,16 @@ def render_video():
                 clips.append(transitionVideo)
 
     final_video = concatenate_videoclips(clips, method="compose") #Unimos todos los clips en uno
+
     #Renderizamo'
     final_video.write_videofile(
         filename = "Final Video.mp4", 
         codec = "libx264",
         audio_bitrate = "320k", 
-        threads = 8, 
+        threads = threadsAvailable, 
         preset = "medium",
-        fps = 30
+        fps = 30,
+        logger = logger
     ) 
 
     #----LIBERAMOS MEMORIA :p -------------------------------------
