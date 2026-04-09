500271fab1181ecc4571931578a4d6726b003ba9
Stefi <stefanocampostrini123@gmail.com>
2026-04-09 01:13:17 -0300

    Squeleton

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ from moviepy import concatenate_videoclips
 import os
 
 clipsPath = r"C:\Users\retra\OneDrive\Escritorio\Main\Code\clips"
+transitionVideo = moviepy.VideoFileClip(os.path.join(r"C:\Users\retra\OneDrive\Escritorio\Main\Code\lazy-clip\transition.mp4")).resized(new_size=(1920, 1080))
 clipsRaw = os.listdir(clipsPath)
 clips = []
 
-for clip in clipsRaw:
+for i,clip in enumerate(clipsRaw):
     if (clip.endswith(".mp4") or clip.endswith(".wav")):
         ruta_completa = os.path.join(clipsPath, clip) 
-        video = moviepy.VideoFileClip(ruta_completa) #Pasar la ruta que es un string al tipo videoFileClip
+        video = moviepy.VideoFileClip(ruta_completa).resized(new_size=(1920, 1080)) #Pasar la ruta que es un string al tipo videoFileClip
         clips.append(video) #Ese video lo metes en la lista de clips a juntar
+        if (i < len(clipsRaw) - 1):
+            clips.append(transitionVideo)
+
 
 final_video = concatenate_videoclips(clips, method="compose") #Unimos todos los clips en uno
 #Renderizamo'
 final_video.write_videofile(
-    filename="Final Video.mp4", 
-    codec="libx264", 
-    threads = 6, 
-    preset="medium",
-    fps=30
+    filename = "Final Video.mp4", 
+    codec = "libx264",
+    audio_bitrate = "320k", 
+    threads = 8, 
+    preset = "medium",
+    fps = 30
     ) 
 
 #----LIBERAMOS MEMORIA :p -------------------------------------
