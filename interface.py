0fae32e4972b11a7b66cb99f5a04b542c5fd0eac
Stefi <stefanocampostrini123@gmail.com>
2026-04-20 10:23:14 -0300

    pequeño avance
    no consigo actualizar la cantidad de clips econtradoos

--- interface.py
+++ interface.py
@@ -2,8 +2,7 @@ import webview
 import threading
 import main
 import json
-
-
+import os
 
 class ProyectosAPI:
 
@@ -11,10 +10,15 @@ class ProyectosAPI:
         result = window.create_file_dialog(webview.FileDialog.FOLDER)
         if result:
             ruta_real = result[0]
-            main.clipsPath = ruta_real
+            main.clipsPath = ruta_real #aca asignamos el resultado de la ruta a la variable de MAIN :o
             ruta_para_js = json.dumps(ruta_real) #Para que JS no se vuelva loco con los caracteres
             window.evaluate_js(f"document.getElementById('path-text').innerText = {ruta_para_js}")
 
+            files = main.os.listdir(main.clipsPath)
+            valid_clips = [f for f in files if f.lower().endswith(('.mp4,' '.wav'))]
+            clipsQuantity = len(valid_clips)
+            print(clipsQuantity)
+            window.evaluate_js(f"document.getElementById('clips-found').innerText = 'Cantidad de clips encontrados: {clipsQuantity}'  ")
 
     def iniciar_render(self):
         threading.Thread(target=self._ejecutar_proceso, daemon=True).start()
@@ -24,9 +28,7 @@ class ProyectosAPI:
         except Exception as e:
             window.evaluate_js(f"alert('Error: {str(e)}')")
 
-
 api = ProyectosAPI()
-
 window = webview.create_window(
     title="Lazy Clip",
     url="index.html",
@@ -34,9 +36,6 @@ window = webview.create_window(
     width=900,
     height=600
 )
-
-
-
 
 if __name__ == "__main__":
     webview.start()
--- main.py
+++ main.py
@@ -10,17 +10,27 @@ threadsAvailable = psutil.cpu_count() - 1 #Cuantos hilos tiene la PC menos uno
 
 
 print(f"Se van a usar {threadsAvailable} hilos")
+
+def selec_folder():
+    if os.listdir(clipsPath) != None:
+        clipsRaw = os.listdir(clipsPath) #OJO, SI NO HAY UN PATH MARCA ERROR, HAY QUE SOLUCIONARLO
+    else:
+        print("NO HAY NINGUNA CARPETA SELECCIONADA")
+    return clipsRaw
+
 def render_video():
     clips = []
-    clipsRaw = os.listdir(clipsPath) #OJO, SI NO HAY UN PATH MARCA ERROR, HAY QUE SOLUCIONARLO
-
-    for i,clip in enumerate(clipsRaw):
-        if (clip.endswith(".mp4") or clip.endswith(".wav")):
+    
+    for i,clip in enumerate(selec_folder()):
+      if (clip.endswith(".mp4") or clip.endswith(".wav")):
             ruta_completa = os.path.join(clipsPath, clip) 
             video = moviepy.VideoFileClip(ruta_completa).resized(new_size=(1920, 1080)) #Pasar la ruta que es un string al tipo videoFileClip
             clips.append(video) #Ese video lo metes en la lista de clips a juntar
-            if (i < len(clipsRaw) - 1):
-                clips.append(transitionVideo)
+            if (i < len(selec_folder()) - 1):
+              clips.append(transitionVideo)
+    
+
+    
 
     final_video = concatenate_videoclips(clips, method="compose") #Unimos todos los clips en uno
 
